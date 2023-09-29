fb3ce7c2ab4f7d8bc43cfa0f617ede5b985a96ba
odanetpro <odanetpro@gmail.com>
2023-09-29 14:59:36 +0900

    add vcr config option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,12 @@
 require 'vcr'
 
-RSpec.configure do |config|
-  config.before(:suite) do
-    VCR.configure do |vcr_config|
-      vcr_config.cassette_library_dir = "spec/vcr_cassettes"
-      vcr_config.hook_into :webmock
-    end
-  end
+VCR.configure do |vcr_config|
+  vcr_config.allow_http_connections_when_no_cassette = false
+  vcr_config.cassette_library_dir = "spec/vcr_cassettes"
+  vcr_config.hook_into :webmock
+end
 
+RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
