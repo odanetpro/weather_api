733a1b91046ccd351fc5c9262fbf53ad9ee3cd22
odanetpro <odanetpro@gmail.com>
2023-10-23 17:32:55 +0900

    add redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module WeatherApi
+  REDIS_URL = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }.freeze
+
   def self.redis
-    @redis ||= Redis::Namespace.new(:weather_api, redis: Redis.new)
+    @redis ||= Redis::Namespace.new(:weather_api, redis: Redis.new(REDIS_URL))
   end
 end
